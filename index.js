d5e12c10fa4dc57fb199dcfd79c98988a43043d3
Kirill Kalinin <kalinin1507.2012@gmail.com>
2024-01-24 17:00:03 +0400

    столкновения

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
 
 const blockWidth = 100;
 const blockHeight = 20;
 const boardWidth = 560;
+const boardHeight = 300;
+const ballDiameter = 20;
+let timerId;
+let xDirection = -2;
+let yDirection = 2;
+let score = 0;
 
 const userStartPos = [230, 10];
 let userCurrentPos = userStartPos;
+
+const ballStartPos = [270, 40];
+const ballCurrentPos = ballStartPos;
 
 class Block {
   constructor(xAxis, yAxis) {
@@ -58,6 +68,12 @@ function drawUser() {
   user.style.right = userCurrentPos[1] + "px";
 }
 
+// Нарисовать шарик
+function drawBall() {
+  ball.style.left = ballCurrentPos[0] + "px";
+  ball.style.bottom = ballCurrentPos[1] + "px";
+}
+
 // Движение юзера
 function moveUser(e) {
   switch (e.key) {
@@ -83,5 +99,74 @@ document.addEventListener("keydown", moveUser);
 // Добавить шарик
 const ball = document.createElement("div");
 ball.classList.add("ball");
+drawBall();
 grid.appendChild(ball);
 
+// Двигать шарик
+function moveBall() {
+  ballCurrentPos[0] += xDirection;
+  ballCurrentPos[1] += yDirection;
+  drawBall();
+  checkForCollisions();
+}
+
+timerId = setInterval(moveBall, 30);
+
+// Проверка на столкновение
+function checkForCollisions() {
+  // Столкновение с блоками
+  for (let i = 0; i < blocks.length; i++) {
+    if (
+      ballCurrentPos[0] > blocks[i].bottomLeft[0] &&
+      ballCurrentPos[0] < blocks[i].bottomRight[0] &&
+      ballCurrentPos[1] + ballDiameter > blocks[i].bottomLeft[1] &&
+      ballCurrentPos[1] < blocks[i].topLeft[1]
+    ) {
+      const allBlocks = Array.from(document.querySelectorAll(".block"));
+      allBlocks[i].classList.remove("block");
+      blocks.splice(i, 1);
+      changeDirection();
+
+      score++;
+      scoreDisplay.innerHTML = score;
+    }
+  }
+
+  // Столкновениеи со стенами
+  if (
+    ballCurrentPos[0] >= boardWidth - ballDiameter ||
+    ballCurrentPos[1] >= boardHeight - ballDiameter ||
+    ballCurrentPos[0] <= 0
+  ) {
+    changeDirection();
+  }
+
+  // Гейм овер
+  if (ballCurrentPos[1] <= 0) {
+    clearInterval(timerId);
+    scoreDisplay.innerHTML = "You lose";
+    document.removeEventListener("keydown", moveUser);
+  }
+}
+
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2;
+    return;
+  }
+
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2;
+    return;
+  }
+
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2;
+    return;
+  }
+
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2;
+    return;
+  }
+}
